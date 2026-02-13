a2f7aeaf8f0ca85f7711020cc932d904cfcbdbe7
Tcardoso280688 <thiago.cardoso@claro.com.br>
2026-02-13 09:41:16 -0300

    Fix formatting and remove extra newlines in utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,7 @@
 from flask_bcrypt import generate_password_hash, check_password_hash
 
-
 def hash_password(plain: str) -> str:
     return generate_password_hash(plain).decode('utf-8')
-
 
 def verify_password(hashed: str, plain: str) -> bool:
     return check_password_hash(hashed, plain)
